3fbdf5d9c589ea9d7cbe36e97dbcd0c42671100a
Augustin Calin <augustincalin@yahoo.com>
2022-01-03 11:31:27 +0100

    initial commit

---
+++ src/app/products/product-list/product-list.component.ts
@@ -0,0 +1,33 @@
+import { Component, Input, Output, EventEmitter, ChangeDetectionStrategy } from '@angular/core';
+import { Product } from '../product';
+
+@Component({
+  selector: 'pm-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class ProductListComponent {
+  pageTitle = 'Products';
+
+  @Input() errorMessage!: string | null;
+  @Input() products!: Product[] | null;
+  @Input() displayCode!: boolean | null;
+  @Input() selectedProduct!: Product | null | undefined;
+  @Output() displayCodeChanged = new EventEmitter<void>();
+  @Output() initializeNewProduct = new EventEmitter<void>();
+  @Output() productWasSelected = new EventEmitter<Product>();
+
+  checkChanged(): void {
+    this.displayCodeChanged.emit();
+  }
+
+  newProduct(): void {
+    this.initializeNewProduct.emit();
+  }
+
+  productSelected(product: Product): void {
+    this.productWasSelected.emit(product);
+  }
+
+}
